6a6955b7c7d9d186b13ccae53f89029a09281219
TecchyToni <68844104+hanchyan@users.noreply.github.com>
2025-10-21 16:07:58 +0900

    shell for submission + my initial thoughts on problem
    Implement initial structure for finding the length of the longest substring without repeating characters.

---
+++ longSubStringNoRep.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        print(len(s))
+
+        return 0
+
+if __name__ == "__main__":
+    s = "abcabcbb"
+    print("Length of Longest Substring:", Solution().lengthOfLongestSubstring(s))
+
+
+
+it would be like put a character from the string in an array
+put the next character in the array if it does not already exist in the array 
+if it already exists, dont add it, get the length of the array and compare it to the current max 
+length and update the max length variable... 
