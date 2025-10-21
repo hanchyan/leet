19de2525114a60956286bd5a1b000892b5d55da3
TecchyToni <68844104+hanchyan@users.noreply.github.com>
2025-10-21 16:08:31 +0900

    commenting things out
    Updated comments to use '#' for clarity.

--- longSubStringNoRep.py
+++ longSubStringNoRep.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
 
 
-it would be like put a character from the string in an array
-put the next character in the array if it does not already exist in the array 
-if it already exists, dont add it, get the length of the array and compare it to the current max 
-length and update the max length variable... 
+# it would be like put a character from the string in an array
+# put the next character in the array if it does not already exist in the array 
+# if it already exists, dont add it, get the length of the array and compare it to the current max 
+# length and update the max length variable... 
