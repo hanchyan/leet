03b26e11b58ec721b01317b5938578a790b7b194
TecchyToni <68844104+hanchyan@users.noreply.github.com>
2025-10-22 14:17:20 +0900

    408/918 test cases passed

--- longSubStringNoRep.py
+++ longSubStringNoRep.py
@@ -1,31 +1,27 @@
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
+        i = 0
         sub = []  # list to store current substring
         long_sub_len = 0
         new_sub_len = 0
-        for i in s:
-            if i not in sub:
-                sub.append(i)
+        while i < len(s):
+            char = s[i]
+            if char not in sub:
+                sub.append(char)
                 new_sub_len = len(sub)
                 long_sub_len = max(new_sub_len, long_sub_len)
-                # print(long_sub_len)
+                i += 1
             else:
                 sub.clear()
                 print("all clear")
+                # i = i -1
+                sub.append(char)
+                i += 1
+
         print(long_sub_len)
-        
         return long_sub_len  # placeholder
 
 if __name__ == "__main__":
     s = "aab"
     # s = "qrstuvwxxabcdefghiijklmnopqrstuv"
     Solution().lengthOfLongestSubstring(s)
-
-
-#it would be like put a character from the string in an array
-#put the next character in the array if it does not already exist in the array 
-#if it already exists, dont add it, get the length of the array and compare it to the current max 
-#length and update the max length variable... 
-
-
-	
